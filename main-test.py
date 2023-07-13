3ef539cbba880762f16ccb3c9ce6373563215a65
LoLynne <lcasale150@gmail.com>
2023-07-13 13:04:19 -0500

    initial commit

---
+++ main-test.py
@@ -0,0 +1,9 @@
+# import the add function and assert that it works correctly
+from main import Add
+
+def TestAdd():
+    assert Add (2,3)==5
+    print ("Add function works correctly")
+
+if __name__=='__main__':
+    TestAdd()    
---
+++ main.py
@@ -0,0 +1,7 @@
+def Add(a, b):
+       return a + b
+def SayHello():
+       print ("hi there")
+
+if __name__=='__main__':
+       SayHello()       
